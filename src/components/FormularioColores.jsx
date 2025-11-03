dbdda88c3ac8bfa6568b0597cc2735db217cf8ab
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-03 10:23:27 -0300

    agregamos card con el color cargado en el input

--- src/components/FormularioColores.jsx
+++ src/components/FormularioColores.jsx
@@ -1,25 +1,42 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ListaColores from './ListaColores';
+import { useState } from 'react';
+
 
 const FormularioColores = () => {
+    const [arrayColores, setArrayColores] = useState([])
+    const [color, setColor] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const colorBuscado = arrayColores.find((itemColor) => itemColor === color.toLowerCase().trim())
+        if (colorBuscado) {
+            return alert("El color ya fue ingresado")
+        }
+        setArrayColores([...arrayColores, color.trim()])
+        // e.target.reset()
+        setColor('')
+
+    }
+
     return (
         <>
             <section className="container border border-secondary shadow p-3 rounded mb-4">
                 <p>Administrar Colores 🎨</p>
-                <Form>
+                <Form onSubmit={handleSubmit}>
                     {/* Contenedor principal: se vuelve flex a partir de md */}
                     <div className="d-md-flex gap-2 align-items-md-center">
                         {/* Grupo del input */}
                         <Form.Group
                             controlId="inputcolor"
-                            className="mb-3 mb-md-0 d-flex gap-2 flex-grow-1 align-items-center"
-                        >
-                            <div className="cuadrado"></div>
+                            className="mb-3 mb-md-0 d-flex gap-2 flex-grow-1 align-items-center">
+                            <div className="border cuadrado" style={{ backgroundColor: color || "transparent" }}></div>
                             <Form.Control
                                 type="text"
                                 className="flex-grow-1"
                                 placeholder="escriba color en inglés"
+                                onChange={(e) => setColor(e.target.value)} value={color}
                             />
                         </Form.Group>
                     </div>
@@ -33,7 +50,7 @@ const FormularioColores = () => {
 
                 </Form>
             </section>
-            <ListaColores></ListaColores>
+                <ListaColores arrayColores={arrayColores}></ListaColores>
         </>
 
     );
--- src/components/ListaColores.jsx
+++ src/components/ListaColores.jsx
@@ -1,26 +1,14 @@
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
+
 import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
+import ItemColor from './itemColor';
 
-
-const ListaColores = () => {
+const ListaColores = ({ arrayColores }) => {
     return (
         <section className="container border border-secondary shadow p-3 rounded">
             <Row xs={1} md={3} className="g-4">
-                <Col>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>Nombre Color</Card.Title>
-                            <article className='d-flex justify-content-center mb-2'>
-                                <div className="cuadrado"></div>
-                            </article>
-                            <div className="d-flex justify-content-end">
-                                <Button variant="secondary">Borrar</Button>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                </Col>
+                {
+                    arrayColores.map((color, indice) => <ItemColor key={indice} color={color}></ItemColor>)
+                }
             </Row>
         </section>
     );
